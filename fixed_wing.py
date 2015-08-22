a46112ebfcd2c44e8bd7a3614c27fa1e15529e71
savadha2 <av.sarath@gmail.com>
2015-08-21 21:26:21 -0700

    resolve fallout from making viewer its own module

--- fixed_wing.py
+++ fixed_wing.py
@@ -1,5 +1,5 @@
 import numpy as np
-from viewer import UAVViewer
+from viewer.viewer import UAVViewer
 from dynamics import FixedWingUAVDynamics
 #import scipy as sp
 #import matplotlib.colors as colors    
