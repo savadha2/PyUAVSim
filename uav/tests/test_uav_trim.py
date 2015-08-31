844d573df1278a6bf472d72f866c037149bf8b2a
Sharath Avadhanam <sharath@utopiacompression.com>
2015-08-31 09:47:41 -0700

    fix: path to config files

--- uav/tests/test_uav_trim.py
+++ uav/tests/test_uav_trim.py
@@ -8,7 +8,7 @@ ax.set_xlim3d(-20, 20)
 ax.set_ylim3d(-20, 20)
 ax.set_zlim3d(0, 40)
 initial_state = [0, 0, 0, 20., 0., 0.0, 0, 0 * np.pi/180, 0, 0, 0, 0.2]
-uav = FixedWingUAV(initial_state, 0, '../configs/zagi.yaml', ax)
+uav = FixedWingUAV(initial_state, 0, '../../configs/zagi.yaml', ax)
 trimmed_state, trimmed_control_inputs = uav.dynamics.trim(10., 0.0, -50.)
 uav.set_state(trimmed_state, 0.)
 uav.set_control_inputs(trimmed_control_inputs)
