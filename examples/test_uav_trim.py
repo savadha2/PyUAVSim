c95c55a9135f2dc338af269af1d4c380defc6020
Sharath Avadhanam <sharath@utopiacompression.com>
2015-12-16 10:17:12 -0800

    resolve fallout from __init__ mod in FixedWingUAV

--- examples/test_uav_trim.py
+++ examples/test_uav_trim.py
@@ -8,8 +8,8 @@ ax.set_xlim3d(-20, 20)
 ax.set_ylim3d(-20, 20)
 ax.set_zlim3d(0, 40)
 initial_state = [0, 0, 0, 20., 0., 0.0, 0, 0 * np.pi/180, 0, 0, 0, 0.2]
-uav = FixedWingUAV(initial_state, 0, '../../configs/zagi.yaml', ax)
-trimmed_state, trimmed_control_inputs = uav.dynamics.trim(10., 0.0, -50.)
+uav = FixedWingUAV('../configs/zagi.yaml', ax)
+trimmed_state, trimmed_control_inputs = uav.dynamics.trim(10., 0.0, -50., epsilon=1e-8, kappa=1e-6, max_iters=25000)
 uav.set_state(trimmed_state, 0.)
 uav.set_control_inputs(trimmed_control_inputs)
 t = uav.dynamics.t0
@@ -19,4 +19,3 @@ for m in range(2400):
     if m%25==0:
         uav.update_view()
         pl.pause(.01)
-
