135537cf93fc704691ee58c10af5f7d63612b52b
Sharath Avadhanam <sharath@utopiacompression.com>
2015-12-17 18:47:08 -0800

    make an app out of fixedwing_uav_trim instead of a script
    still needs cleanup

--- examples/fixedwing_uav_trim.py
+++ examples/fixedwing_uav_trim.py
@@ -2,18 +2,68 @@ import numpy as np
 import mpl_toolkits.mplot3d as a3
 import pylab as pl
 from uav.fixed_wing import FixedWingUAV#, FixedWingUAVDynamics
+from viewer.viewer import UAVViewer
 
+class AppFixedWingUAVTrim(FixedWingUAV):
+    fuse_l1 = 5.
+    fuse_l2 = 2.5
+    fuse_l3 = 10.
+    fuse_h = 3.
+    fuse_w = 3.
+    wing_w = 15.
+    wing_l = 3.
+    tail_h = 4.
+    tailwing_w = 7.5
+    tailwing_l = 1.5
+    def __init__(self, x0, t0, config, ax):
+        super(AppFixedWingUAVTrim, self).__init__(x0, t0, config)
+        self.vertices = np.matrix(np.zeros((16, 3), dtype = np.double))
+        self.vertices[0, :] =  [self.fuse_l1, 0.0, 0.0]
+        self.vertices[1, :] = [self.fuse_l2, 0.5*self.fuse_w, 0.5*self.fuse_h]
+        self.vertices[2, :] = [self.fuse_l2, -0.5*self.fuse_w, 0.5*self.fuse_h]
+        self.vertices[3, :] = [self.fuse_l2, -0.5*self.fuse_w, -0.5*self.fuse_h]
+        self.vertices[4, :] = [self.fuse_l2, 0.5*self.fuse_w, -0.5*self.fuse_h]
+        self.vertices[5, :] = [-self.fuse_l3, 0., 0.]
+        self.vertices[6, :] = [0, 0.5 * self.wing_w, 0.0]
+        self.vertices[7, :] = [-self.wing_l, 0.5 * self.wing_w, 0.0]
+        self.vertices[8, :] = [-self.wing_l, -0.5 * self.wing_w, 0.0]
+        self.vertices[9, :] = [0, -0.5 * self.wing_w, 0.0]
+        self.vertices[10, :] = [-(self.fuse_l3 - self.tailwing_l), 0.5 * self.tailwing_w, 0.]
+        self.vertices[11, :] = [-self.fuse_l3, 0.5 * self.tailwing_w, 0.]
+        self.vertices[12, :] = [-self.fuse_l3, -0.5 * self.tailwing_w, 0.]
+        self.vertices[13, :] = [-(self.fuse_l3 - self.tailwing_l), -0.5 * self.tailwing_w, 0.]
+        self.vertices[14, :] = [-(self.fuse_l3 - self.tailwing_l), 0.0, 0.]
+        self.vertices[15, :] = [-self.fuse_l3, 0.0, -self.tail_h]
+        self.vertices = 0.3048 * self.vertices
+        
+        self.nose = [[0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 1, 4]]
+        self.fuselage = [[5, 3, 2], [5, 2, 1], [5, 3, 4], [5, 4, 1]]
+        self.wing = [[6, 7, 8, 9]]
+        self.tail_wing = [[10, 11, 12, 13]]
+        self.tail = [[5, 14, 15]]
+        self.faces = [self.nose, self.fuselage, self.wing, self.tail_wing, self.tail]
+        self.viewer = UAVViewer(ax, (self.rotate(x0[6:9]) + x0[0:3]), self.faces, ['r', 'g', 'g', 'g', 'y'])
+        
+    def update_view(self):
+        vertices = self.rotate(self.dynamics.x[6:9]) + self.dynamics.x[0:3]
+        self.viewer.update(vertices)
+    
+    def trim(self, Va, gamma, radius, max_iters):
+        trimmed_state, trimmed_control_inputs = self.dynamics.trim(Va, gamma, radius, epsilon=1e-8, kappa=1e-6, max_iters=max_iters)
+        return trimmed_state, trimmed_control_inputs
+    
+    
 ax = a3.Axes3D(pl.figure(1))
 ax.set_xlim3d(-20, 20)
 ax.set_ylim3d(-20, 20)
 ax.set_zlim3d(0, 40)
-initial_state = [0, 0, 0, 20., 0., 0.0, 0, 0 * np.pi/180, 0, 0, 0, 0.2]
-uav = FixedWingUAV('../configs/zagi.yaml', ax)
-trimmed_state, trimmed_control_inputs = uav.dynamics.trim(10., 0.0, -50., epsilon=1e-8, kappa=1e-6, max_iters=25000)
+initial_state = [0, 0, 0, 10., 0., 0.0, 0, 0 * np.pi/180, 0, 0, 0, 0.2]
+uav = AppFixedWingUAVTrim(initial_state, 0, '../configs/zagi.yaml', ax)
+t = uav.dynamics.t0
+trimmed_state, trimmed_control_inputs = uav.trim(10., 0., 50, 25000)
 uav.set_state(trimmed_state, 0.)
 uav.set_control_inputs(trimmed_control_inputs)
-t = uav.dynamics.t0
-pl.show()
+
 npoints = 2400
 v = np.zeros((2400,), dtype = np.double)
 gamma = np.zeros((2400,), dtype = np.double)
@@ -22,6 +72,7 @@ y = np.zeros((2400,), dtype = np.double)
 z = np.zeros((2400,), dtype = np.double)
 t = np.zeros((2400,), dtype = np.double)
 
+pl.show()
 for m in range(npoints):
     uav.update_state(dt = 1/200.)
     v[m] = np.linalg.norm(uav.dynamics.x[3:6])
