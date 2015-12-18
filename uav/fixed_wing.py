8c6b0fc75d4793a80ed0cc6530ab868825788080
Sharath Avadhanam <sharath@utopiacompression.com>
2015-12-17 18:43:39 -0800

    decouple viewer from fixedwinguav

--- uav/fixed_wing.py
+++ uav/fixed_wing.py
@@ -1,20 +1,8 @@
 import numpy as np
-from viewer.viewer import UAVViewer
 from dynamics import FixedWingUAVDynamics
 #import scipy as sp
 #import matplotlib.colors as colors    
 class FixedWingUAV:
-    fuse_l1 = 5.
-    fuse_l2 = 2.5
-    fuse_l3 = 10.
-    fuse_h = 3.
-    fuse_w = 3.
-    wing_w = 15.
-    wing_l = 3.
-    tail_h = 4.
-    tailwing_w = 7.5
-    tailwing_l = 1.5
-    
     def __init__(self, dynamics_config_file, ax):
         self.vertices = np.matrix(np.zeros((16, 3), dtype = np.double))
 #        self.x = np.zeros((12,), dtype = np.double)
@@ -22,31 +10,6 @@ class FixedWingUAV:
 #        self.t = t0
         x0 = np.zeros((12,), dtype = np.double)
         t0 = 0
-        self.vertices[0, :] =  [self.fuse_l1, 0.0, 0.0]
-        self.vertices[1, :] = [self.fuse_l2, 0.5*self.fuse_w, 0.5*self.fuse_h]
-        self.vertices[2, :] = [self.fuse_l2, -0.5*self.fuse_w, 0.5*self.fuse_h]
-        self.vertices[3, :] = [self.fuse_l2, -0.5*self.fuse_w, -0.5*self.fuse_h]
-        self.vertices[4, :] = [self.fuse_l2, 0.5*self.fuse_w, -0.5*self.fuse_h]
-        self.vertices[5, :] = [-self.fuse_l3, 0., 0.]
-        self.vertices[6, :] = [0, 0.5 * self.wing_w, 0.0]
-        self.vertices[7, :] = [-self.wing_l, 0.5 * self.wing_w, 0.0]
-        self.vertices[8, :] = [-self.wing_l, -0.5 * self.wing_w, 0.0]
-        self.vertices[9, :] = [0, -0.5 * self.wing_w, 0.0]
-        self.vertices[10, :] = [-(self.fuse_l3 - self.tailwing_l), 0.5 * self.tailwing_w, 0.]
-        self.vertices[11, :] = [-self.fuse_l3, 0.5 * self.tailwing_w, 0.]
-        self.vertices[12, :] = [-self.fuse_l3, -0.5 * self.tailwing_w, 0.]
-        self.vertices[13, :] = [-(self.fuse_l3 - self.tailwing_l), -0.5 * self.tailwing_w, 0.]
-        self.vertices[14, :] = [-(self.fuse_l3 - self.tailwing_l), 0.0, 0.]
-        self.vertices[15, :] = [-self.fuse_l3, 0.0, -self.tail_h]
-        self.vertices = 0.3048 * self.vertices
-        
-        self.nose = [[0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 1, 4]]
-        self.fuselage = [[5, 3, 2], [5, 2, 1], [5, 3, 4], [5, 4, 1]]
-        self.wing = [[6, 7, 8, 9]]
-        self.tail_wing = [[10, 11, 12, 13]]
-        self.tail = [[5, 14, 15]]
-        self.faces = [self.nose, self.fuselage, self.wing, self.tail_wing, self.tail]
-        self.viewer = UAVViewer(ax, (self.rotate(x0[6:9]) + x0[0:3]), self.faces, ['r', 'g', 'g', 'g', 'y'])
         self.dynamics = FixedWingUAVDynamics(x0, t0, 0.001, dynamics_config_file)
     
     def move_to(self, pos):
@@ -80,10 +43,6 @@ class FixedWingUAV:
     def rotate(self, ypr_rad):
         vertices = self.vertices * self.R_bv(ypr_rad)
         return vertices
-    
-    def update_view(self):
-        vertices = self.rotate(self.dynamics.x[6:9]) + self.dynamics.x[0:3]
-        self.viewer.update(vertices)
     
     def update_state(self, dt):
          if self.dynamics.control_inputs is not None:
