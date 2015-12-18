2c42a042e3799f074ad509c1f3138d0b4fa05873
Sharath Avadhanam <sharath@utopiacompression.com>
2015-12-17 17:33:45 -0800

    RollAttitudeHold is child of PID

--- autopilot/roll_attitude_hold.py
+++ autopilot/roll_attitude_hold.py
@@ -4,17 +4,17 @@ Created on Wed Dec 16 14:40:46 2015
 
 @author: sharath
 """
-from controllers.pid import PID as pid_controller
+from controllers.pid import PID
 import yaml
 
-class PID:
+class RollAttitudeHold(PID):
     def __init__(self, config, Ts):
         self.kp = 1
         self.kd = 0
         self.ki = 0
         self.limit = 0
         self.tau = 0
-        self.roll_attitude_hold = pid_controller(self.kp_phi, self.ki_phi, self.kd, self.phi_limit, Ts, self.tau)
+        super(RollAttitudeHold, self).__init__(self.kp, self.ki, self.kd, self.limit, Ts, self.tau)
     
     def roll_attitude_hold(self, phi_c, phi):
         delta_a = self.roll_attitude_hold.compute_control_input(phi_c, phi)
