7ee6a7eb59858925d74c39ce5d95d4505b297ac0
Sharath Avadhanam <sharath@utopiacompression.com>
2016-03-23 11:02:51 -0700

    add f, h, F, H for the Position, heading and windspeed estimator

--- apps/fixedwing_uav_state_estimation.py
+++ apps/fixedwing_uav_state_estimation.py
@@ -72,11 +72,104 @@ class RollPitchEstimator(EKF):
 class PositionHeadingEstimator(EKF):
     #u = Va, q, r, phi, theta
     #x = pn, pe, Vg, chi, wn, we, psi
+    g = 9.81
     def f(self, u):
-        pass
+        Vg = self.x[2]
+        chi = self.x[3]
+        wn = self.x[4]
+        we = self.x[5]
+        psi = self.x[6]        
+        Va = u[0]        
+        q = u[1]
+        r = u[2]
+        roll = u[3]
+        pitch = u[4]
+        sr = np.sin(roll)
+        cr = np.cos(roll)
+        cp = np.cos(pitch)
+        psi_dot = q * sr/cp + r * cr/cp
+        _f = np.zeros((7,), dtype=np.double)
+        _f[0] = Vg * np.cos(chi)
+        _f[1] = Vg * np.sin(chi)
+        _f[2] = (Va * np.cos(psi) + wn) * -Va/Vg * psi_dot * np.sin(psi) 
+        + (Va * np.sin(psi) + we)* Va/Vg * psi_dot * np.cos(psi)
+        _f[3] = self.g/Vg * np.tan(roll) * np.cos(chi - psi)
+        _f[4] = 0.0
+        _f[5] = 0.0
+        _f[6] = q * sr/cp + r * cr/cp
+        return _f
+    
     def h(self, u):
-        pass
+        pn = self.x[0]
+        pe = self.x[1]
+        Vg = self.x[2]
+        chi = self.x[3]
+        wn = self.x[4]
+        we = self.x[5]
+        psi = self.x[6]
+        Va = u[0]
+        _h = np.zeros((6,), dtype = np.double)
+        _h[0] = pn
+        _h[1] = pe
+        _h[2] = Vg
+        _h[3] = chi
+        _h[4] = Va * np.cos(psi) + wn - Vg * np.cos(chi)
+        _h[5] = Va * np.sin(psi) + we - Vg * np.sin(chi)        
+    
     def F(self, u):
-        pass
+        Vg = self.x[2]
+        chi = self.x[3]
+        wn = self.x[4]
+        we = self.x[5]
+        psi = self.x[6]        
+        Va = u[0]        
+        q = u[1]
+        r = u[2]
+        roll = u[3]
+        pitch = u[4]
+        sr = np.sin(roll)
+        cr = np.cos(roll)
+        tr = np.tan(roll)
+        cp = np.cos(pitch)
+        psi_dot = q * sr/cp + r * cr/cp
+        schi = np.sin(chi)
+        cchi = np.cos(chi)
+        spsi = np.sin(psi)
+        cpsi = np.cos(psi)
+        Vg_dot = 1/Vg * (Va * cpsi + wn) * (-Va * psi_dot * spsi) 
+        + 1/Vg * (Va * spsi + we) * (Va * psi_dot * cpsi) 
+        _F = np.zeros((7, 7), dtype = np.double)
+        _F[0, 2] = cchi
+        _F[0, 3] = -Vg * schi
+        _F[1, 2] = schi
+        _F[1, 3] = Vg * cchi
+        _F[2, 2] = -Vg_dot/Vg
+        _F[2, 4] = -psi_dot * Va * spsi
+        _F[2, 5] = psi_dot * Va * cpsi
+        _F[2, 6] = -psi_dot * Va/Vg * (wn * cpsi + we * spsi)
+        _F[3, 2] = -self.g/Vg**2 * tr * np.cos(chi - psi)
+        _F[3, 3] = self.g/Vg * tr * np.sin(chi - psi)
+        _F[3, 6] = self.g/Vg * tr * np.sin(chi - psi)
+        return _F
+    
     def H(self, u):
-        pass    
+        Vg = self.x[2]
+        chi = self.x[3]        
+        psi = self.x[6]
+        schi = np.sin(chi)
+        cchi = np.cos(chi)
+        Va = u[0]   
+        _H = np.zeros((6, 7), dtype = np.double)
+        _H[0, 0] = 1
+        _H[1, 1] = 1
+        _H[2, 2] = 1
+        _H[3, 3] = 1
+        _H[4, 2] = -cchi
+        _H[4, 3] = Vg * schi
+        _H[4, 4] = 1
+        _H[4, 6] = -Va * np.sin(psi)
+        _H[5, 2] = -schi
+        _H[5, 3] = Vg * cchi
+        _H[5, 5] = 1
+        _H[5, 6] = Va * np.cos(psi)
+        return _H
